e93c309ca435a83686106f205ccd0b9da1090f90
Camila Xavier <camilaxmedeiros@gmail.com>
2024-10-09 17:54:00 -0300

    tomaaaa essa merda

---
+++ VisionTest/VisionTest/ContentView.swift
@@ -0,0 +1,30 @@
+////
+////  ContentView.swift
+////  VisionTest
+////
+////  Created by Camila Xavier de Medeiros on 08/10/24.
+////
+//
+//import SwiftUI
+//
+//import SwiftUI
+//
+//struct ContentView: View {
+//    @State private var mouthStatus: String = "Status da boca"  // Estado inicial
+//    
+//    var body: some View {
+//        VStack {
+//            Text(mouthStatus)  // Mostra o estado da boca
+//                .font(.title)
+//                .padding()
+//            
+//            CameraView(mouthStatus: $mouthStatus)  // A câmera será exibida aqui
+//                .frame(height: 400)  // Defina o tamanho que você quiser para a visualização da câmera
+//            
+//            Spacer()
+//        }
+//    }
+//}
+//
+//
+//
---
+++ VisionTest/VisionTest/VisionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTestApp.swift
+//  VisionTest
+//
+//  Created by Camila Xavier de Medeiros on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
